de78a045c0626891e1e87470f883506418300791
Matt Summersgill <matt.summersgill@hpidc.com>
2017-06-22 14:40:26 -0500

    Fixed min frequency

--- src/RcppWaveletTest.cpp
+++ src/RcppWaveletTest.cpp
@@ -10,15 +10,16 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 arma::cx_mat RcppWaveletTest(std::vector<double> x) {
   float samplerate_hz(1);
-  float frequency_min = 0.3125;
+  float frequency_min = 0.03125;
   float frequency_max = 0.5;
-  float bands_per_octave = 32;
+  float bands_per_octave = 16;
+  
   wavelet::Filterbank cwt(samplerate_hz,
                           frequency_min,
                           frequency_max,
                           bands_per_octave);
   std::size_t numbands(cwt.size());
-  //cwt.reset(); // Reset processing
+  cwt.reset(); // Reset processing
   return cwt.process(x);
   
 }
