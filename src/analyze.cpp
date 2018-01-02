e78c4fb0af286b69712391f48ea3ecafe2adae19
Matt Summersgill <matt.summersgill@hpidc.com>
2018-01-02 16:31:02 -0600

    Condense to a single analyze function

--- src/analyze.cpp
+++ src/analyze.cpp
@@ -7,14 +7,16 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 Rcpp::List analyze(std::vector<double> x,
-                             float bands_per_octave = 8,
-                             float frequency_min = 0.001953125,
-                             float frequency_max = 0.5,
-                             float samplerate_hz = 1,
-                             std::string mother_wavelet = "MORLET",
-                             float morlet_carrier = 5,
-                             unsigned int paul_order = 2,
-                             std::string optimisation = "NONE") {
+                   float bands_per_octave = 8,
+                   float frequency_min = 0.001953125,
+                   float frequency_max = 0.5,
+                   float samplerate_hz = 1,
+                   std::string mother_wavelet = "MORLET",
+                   float morlet_carrier = 5,
+                   unsigned int paul_order = 2,
+                   int cores = 1,
+                   std::string optimisation = "NONE") {
+  
   wavelet::Filterbank cwt(samplerate_hz,
                           frequency_min,
                           frequency_max,
@@ -43,7 +45,14 @@ Rcpp::List analyze(std::vector<double> x,
   
   std::size_t numbands(cwt.size());
   cwt.reset();
-  arma::cx_mat scalogram = cwt.process(x);
+  
+  //arma::cx_mat *scalogram = NULL;
+  //if (cores > 2) {
+    arma::cx_mat scalogram = cwt.processParallel(x, cores);
+  //} else {
+  //  arma::cx_mat scalogram = cwt.process(x);
+  //}
+  
   std::vector<double> frequencies = cwt.frequencies;
   
   arma::vec periods_arma = 1/arma::conv_to<arma::vec>::from(frequencies); 
@@ -51,6 +60,7 @@ Rcpp::List analyze(std::vector<double> x,
   periods.attr("dim") = R_NilValue;
   
   std::string configuration = cwt.info();
+  
   return Rcpp::List::create(Rcpp::Named("signal")=x,
                             Rcpp::Named("scalogram")=scalogram,
                             Rcpp::Named("frequencies")=frequencies,
@@ -58,52 +68,3 @@ Rcpp::List analyze(std::vector<double> x,
                             Rcpp::Named("configuration")=configuration);
 }
 
-// [[Rcpp::export]]
-Rcpp::List analyzeParallel(std::vector<double> x,
-                           float bands_per_octave = 8,
-                           float frequency_min = 0.001953125,
-                           float frequency_max = 0.5,
-                           float samplerate_hz = 1,
-                           int cores = 1,
-                           std::string mother_wavelet = "MORLET",
-                           float morlet_carrier = 5,
-                           unsigned int paul_order = 2,
-                           std::string optimisation = "NONE") {
-  
-  wavelet::Filterbank cwt(samplerate_hz,
-                          frequency_min,
-                          frequency_max,
-                          bands_per_octave);
-  
-  if (mother_wavelet == "MORLET") {
-    cwt.setAttribute("family",wavelet::Family::MORLET);
-    cwt.setAttribute("omega0",morlet_carrier);
-  } else if (mother_wavelet == "PAUL") {
-    cwt.setAttribute("family",wavelet::Family::PAUL);
-    cwt.setAttribute("order",paul_order);
-  } else {
-    throw std::runtime_error("Invalid mother_wavelet. Try 'MORLET' or 'PAUL'");
-  }
-  
-  
-  if (optimisation == "NONE") {
-    cwt.setAttribute("optimisation",wavelet::Filterbank::Optimisation::NONE);
-  } else if (optimisation == "STANDARD") {
-    cwt.setAttribute("optimisation",wavelet::Filterbank::Optimisation::STANDARD);
-  } else if (optimisation == "AGRESSIVE") {
-    cwt.setAttribute("optimisation",wavelet::Filterbank::Optimisation::AGRESSIVE);
-  } else {
-    throw std::runtime_error("Invalid optimisation. Try 'NONE', 'STANDARD', or 'AGGRESSIVE'");
-  }
-  
-  std::size_t numbands(cwt.size());
-  cwt.reset();
-  arma::cx_mat scalogram = cwt.processParallel(x, cores);
-  std::vector<double> frequencies = cwt.frequencies;
-  arma::vec periods = 1/arma::conv_to<arma::vec>::from(frequencies); 
-  std::string configuration = cwt.info();
-  return Rcpp::List::create(Rcpp::Named("signal")=x,
-                            Rcpp::Named("scalogram")=scalogram,
-                            Rcpp::Named("periods")=periods,
-                            Rcpp::Named("configuration")=configuration);
-}
